cfa845138cb706365b60878155c04c7fa53f70f2
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-03-17 11:05:09 +0900

    Merge branch 'project-mvp-back' into develop

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -11,8 +11,4 @@ const db = mongoose.connection;
 db.on("connected", () => console.log("정상적으로 MongoDB 서버에 연결되었습니다.  " + DB_URL));
 db.on("error", (error) => console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error));
 
-<<<<<<< back/src/db/index.js
-export { User, Project };
-=======
-export { User, Education };
->>>>>>> back/src/db/index.js
+export { User, Education, Project };

c73bb38f1efcca7b829ea9edda9a70960497e0f2
jeongmin <leejm648@gmail.com>
2022-03-17 11:23:31 +0900

    Feat: '추가 프로젝트 MVP 생성(Create), 조회(Read)'

---
+++ front/src/components/project/ProjectEditForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProjectEditForm = ({ currentproject, setProjects, setIsEditing }) => {
+
+
+  return (
+    <div>ProjectEditForm</div>
+  );
+};
+
+export default ProjectEditForm;
---
+++ front/src/components/project/Projects.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import * as Api from "../../api";
+import { Card, Button, Row, Col } from "react-bootstrap";
+import Project from "./Project";
+import ProjectAddForm from "./ProjectAddForm";
+
+const Projects = ({ portfolioOwnerId, isEditable }) => {
+    
+    const [projects, setProjects] = useState([])
+    const [isAdding, setIsAdding] = useState(false)
+
+    // 조회 ("projectlist/유저id"로 GET 요청하고, response의 data로 projects를 세팅함.)
+    useEffect(() => {
+        Api.get("projectlist", portfolioOwnerId).then((res) => setProjects(res.data))
+    }, [portfolioOwnerId])
+
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>프로젝트</Card.Title>
+                {projects.map((project) => (
+                    <Project 
+                        key={project.id}
+                        project={project}
+                        setProjects={setProjects}
+                        isEditable={isEditable}
+                    />
+                ))}
+                {isEditable && (
+                    <Row className="mt-3 text-center mb-4">
+                        <Col sm={{ span: 20 }}>
+                            <Button onClick={() => setIsAdding(true)}>+</Button>
+                        </Col>
+                    </Row>
+                )}
+                {isAdding && (
+                    <ProjectAddForm
+                        portfolioOwnerId={portfolioOwnerId}
+                        setProjects={setProjects}
+                        setIsAdding={setIsAdding}
+                    />
+                )}
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default Projects;

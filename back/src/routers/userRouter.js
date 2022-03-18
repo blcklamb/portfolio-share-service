41667fae2cbf2fb280c906ea0ca01f18259653fd
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-03-18 11:22:31 +0900

    Fix: user edit router :id to current

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -83,11 +83,10 @@ userAuthRouter.get("/user/current", login_required, async function (req, res, ne
     }
 });
 
-userAuthRouter.put("/users/:id", login_required, uploadImage.single("image"), async function (req, res, next) {
+userAuthRouter.put("/users/current", login_required, uploadImage.single("image"), async function (req, res, next) {
     try {
+        // jwt토큰에서 추출된 사용자 id를 가지고 db에서 사용자 정보를 찾음.
         const user_id = req.currentUserId;
-        // URI로부터 사용자 id를 추출함.
-        const { id } = req.params;
         // body data 로부터 업데이트할 사용자 정보를 추출함.
         const name = req.body.name ?? null;
         const password = req.body.password ?? null;

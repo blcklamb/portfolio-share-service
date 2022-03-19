5c8ca8ab6a216039fb42262444016f6ca3cdea9f
Hyejin Eom <hjeom.dev@gmail.com>
2022-03-19 18:53:21 +0900

    Style: 학력, 블로그 코드 포맷 변경 및 불필요한 검사 조건문 삭제

--- back/src/services/blogService.js
+++ back/src/services/blogService.js
@@ -5,16 +5,19 @@ class blogService {
   static async addBlog({ user_id, service, url }) {
     // 중복 확인
     const preBlog = await this.getBlogs({ user_id });
+
     for (let i = 0; i < preBlog.length; i++) {
       if (preBlog[i]?.service === service && preBlog[i]?.url === url) {
         const errorMessage = "이미 입력한 블로그입니다.";
         return { errorMessage };
       }
     }
-    // 저장
+
+    // 블로그 생성
     const newBlog = { id: uuidv4(), user_id, service, url };
     const createdNewBlog = await Blog.create(newBlog);
 
+    // 블로그 생성 실패한 경우
     if (createdNewBlog.errorMessage) {
       const errorMessage = "블로그 저장에 실패하였습니다.";
       return { errorMessage };
@@ -23,14 +26,11 @@ class blogService {
     return createdNewBlog;
   }
 
-  static async getBlogs({ user_id }) {
-    const foundBlogs = await Blog.findByUserId({ user_id });
-    return foundBlogs;
-  }
-
   static async getBlog({ id }) {
+    // 블로그 조회
     const foundBlog = await Blog.findById({ id });
     
+    // 블로그 조회 실패한 경우
     if (!foundBlog) {
       const errorMessage = "존재하지 않는 블로그 항목 입니다.";
       return { errorMessage };
@@ -39,38 +39,54 @@ class blogService {
     return foundBlog;
   }
 
+  static async getBlogs({ user_id }) {
+    // 블로그 조회
+    const foundBlogs = await Blog.findByUserId({ user_id });
+    return foundBlogs;
+  }
+
   static async setBlog({ id, user_id, service, url }) {
+    // 존재하는 정보인지 확인
     const foundBlog = await this.getBlog({ id });
+
     if(foundBlog.errorMessage) {
         return foundBlog.errorMessage;
     }
 
+    // 블로그 수정
     const updatedBlog = await Blog.update({ id, user_id, service, url });
 
+    // 블로그 수정 실패시 에러메세지
     if (!updatedBlog) {
       const errorMessage = "블로그 수정에 실패했습니다.";
       return { errorMessage };
     }
 
+    // 블로그 수정 성공시 에러메세지 제거
     updatedBlog.errorMessage = null;
 
     return updatedBlog;
   }
 
   static async deleteBlog({ id }) {
+    // 존재하는 정보인지 확인
     const foundBlog = await this.getBlog({ id });
+
     if(!foundBlog) {
         const errorMessage = "이미 존재하지 않은 블로그 항목 입니다.";
         return { errorMessage };
     }
 
+    // 블로그 삭제
     const deletedBlog = await Blog.delete({ id });
     
+    // 블로그 삭제 실패시 에러메시지
     if (deletedBlog.deletedCount === 0) {
       const errorMessage = "블로그 삭제에 실패했습니다.";
       return { errorMessage };
     }
 
+    // 블로그 삭제 성공시 에러메시지 제거
     deletedBlog.errorMessage = null;
     
     return deletedBlog;

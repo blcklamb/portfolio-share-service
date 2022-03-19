11d8d912f7529c845d107991a67423280cb9a0b1
jeongmin <leejm648@gmail.com>
2022-03-19 17:58:47 +0900

    Feat: '추가 자격증 MVP 삭제(Delete)'

--- front/src/components/certificate/CertificateCard.js
+++ front/src/components/certificate/CertificateCard.js
@@ -1,33 +1,60 @@
 import React from "react";
 import { Card, Row, Col, Button } from "react-bootstrap";
+import * as Api from "../../api";
 
-const CertificateCard = ({ certificate, isEditable, setIsEditing }) => {
+const CertificateCard = ({ certificate, setCertificates, isEditable, setIsEditing }) => {
+  
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const delete_content = certificate.id
 
-    return (
-        <Card.Text>
-        <Row className="align-items-center">
-          <Col>
-            <span>{certificate.title}</span>
-            <br />
-            <span className="text-muted">{certificate.description}</span>
-            <br />
-            <span className="text-muted">{certificate.when_date.slice(0, 10)}</span>
+    await Api.delete(`certificates/${delete_content}`);
+
+    // "certificatelist/유저id" 엔드포인트로 get요청함.
+    const res = await Api.get("certificatelist", certificate.user_id);
+    alert("삭제되었습니다.")
+
+    setCertificates(res.data);
+  }
+
+  return (
+    <Card.Text>
+    <Row className="align-items-center">
+      <Col>
+        <span>{certificate.title}</span>
+        <br />
+        <span className="text-muted">{certificate.description}</span>
+        <br />
+        <span className="text-muted">{certificate.when_date.slice(0, 10)}</span>
+      </Col>
+      {isEditable && (
+        <>
+          <Col md="auto">
+            <Button
+              variant="outline-info"
+              size="sm"
+              onClick={() => setIsEditing((prev) => !prev)}
+              className="mr-3"
+            >
+              편집
+            </Button>
           </Col>
-          {isEditable && (
-            <Col xs lg="1">
-              <Button
-                variant="outline-info"
-                size="sm"
-                onClick={() => setIsEditing((prev) => !prev)}
-                className="mr-3"
-              >
-                편집
-              </Button>
-            </Col>
-          )}
-        </Row>
-      </Card.Text>
-    )
+          <Col md="auto">
+            <Button
+              variant="outline-danger"
+              size="sm"
+              onClick={(e) => {if(window.confirm("해당 자격증을 삭제하시겠습니까?")){handleDelete(e)}}}
+              className="mr-3"
+            >
+              삭제
+            </Button>
+          </Col>
+      </>  
+      )}
+    </Row>
+  </Card.Text>
+  )
 }
 
 export default CertificateCard

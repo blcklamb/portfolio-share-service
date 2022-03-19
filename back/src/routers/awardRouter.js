75ad9133fc2e4a361a9878a6c2fc0497310f059f
Baejuyoung <baejuyoung49@gmail.com>
2022-03-19 19:02:35 +0900

    Feat: 수상 이력 중복 확인하기 & 주석 추가

--- back/src/routers/awardRouter.js
+++ back/src/routers/awardRouter.js
@@ -19,7 +19,7 @@ awardAuthRouter.post("/award/create", async function (req, res, next) {
     const title = req.body.title;
     const description = req.body.description;
 
-    // 위 데이터를 유저 db에 추가하기
+    // 위 데이터를 수상 이력 db에 추가하기
     const newAward = await awardAuthService.addAward({
       user_id,
       title,
@@ -38,12 +38,15 @@ awardAuthRouter.post("/award/create", async function (req, res, next) {
 
 awardAuthRouter.get('/awards/:id', async function (req, res, next) {
   try { 
+    // req에서 id 가져오고 id로 수상 이력 조회
     const awardId = req.params.id;
     const award = await awardAuthService.getAward({ awardId });
 
     if (award.errorMessage) {
       throw new Error(award.errorMessage);
     }
+
+    // 조회된 수상 이력 보내기
     res.status(200).send(award);
   } catch (error) {
     next(error);
@@ -52,12 +55,15 @@ awardAuthRouter.get('/awards/:id', async function (req, res, next) {
 
 awardAuthRouter.get('/awardlist/:user_id', async function (req, res, next) {
   try {
+    // req에서 user_id 가져오고 user_id로 수상 이력 조회
     const user_id = req.params.user_id;
     const awards = await awardAuthService.getAwards({ user_id });
 
     if (awards.errorMessage) {
       throw new Error(awards.errorMessage);
     }
+
+    // 조회된 수상 이력 보내기
     res.status(200).send(awards)
   } catch (error) {
     next(error);
@@ -66,15 +72,19 @@ awardAuthRouter.get('/awardlist/:user_id', async function (req, res, next) {
 
 awardAuthRouter.put("/awards/:id", async function (req, res, next) {
   try {
+    // req에서 데이터 가져오기
     const awardId = req.params.id;
     const title = req.body.title;
     const description = req.body.description;
     
+    // 수상 이력 내용 수정하기
     const updatedAward = await awardAuthService.setAward({ awardId, title, description});
 
     if (updatedAward.errorMessage) {
       throw new Error(updatedAward.errorMessage);
     }
+
+    // 수정한 결과 보내기
     res.status(200).send(updatedAward)
   } catch (error) {
     next(error);
@@ -83,20 +93,18 @@ awardAuthRouter.put("/awards/:id", async function (req, res, next) {
 
 awardAuthRouter.delete("/awards/:id", async function (req, res, next) {
   try {
+    // req에서 데이터 가져오기
     const awardId = req.params.id;
-    const currentUser = req.currentUserId;
     const award = await awardAuthService.getAward({ awardId });
 
-    if (String(currentUser) !== String(award.user_id)) {
-      throw new Error("접근 권한이 없습니다.")
-    }
-
+    // 수상 이력 삭제하기
     const deletedAward = await awardAuthService.deleteAward({ awardId });
 
     if (deletedAward.errorMessage) {
       throw new Error(deletedAward.errorMessage);
     }
 
+    // 삭제한 결과 보내기
     res.status(200).send("성공적으로 수상 이력이 삭제되었습니다.")
   } catch (error) {
     next(error);

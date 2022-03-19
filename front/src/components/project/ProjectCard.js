dec4c1df423717c1e82aa2902ed334d4da1d7a0a
jeongmin <leejm648@gmail.com>
2022-03-19 19:06:08 +0900

    Feat: '추가 프로젝트 MVP 삭제(Delete)'

--- front/src/components/project/ProjectCard.js
+++ front/src/components/project/ProjectCard.js
@@ -1,30 +1,58 @@
 import React from "react";
 import { Card, Row, Col, Button } from "react-bootstrap";
+import * as Api from "../../api";
 
-const ProjectCard = ({ project, isEditable, setIsEditing }) => {
+const ProjectCard = ({ project, setProjects, isEditable, setIsEditing }) => {
+
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const delete_content = project.id
+
+    await Api.delete(`projects/${delete_content}`);
+
+    // "projectlist/유저id" 엔드포인트로 get요청함.
+    const res = await Api.get("projectlist", project.user_id);
+    alert("삭제되었습니다.")
+
+    setProjects(res.data);
+  }
 
     return (
-        <Card.Text>
+      <Card.Text>
         <Row className="align-items-center">
           <Col>
             <span>{project.title}</span>
             <br />
             <span className="text-muted">{project.description}</span>
             <br />
-            <span className="text-muted me-3">{project.from_date.slice(0, 10)}</span>
-            <span className="text-muted">{project.to_date.slice(0, 10)}</span>
+            <span className="text-muted me-1">{project.from_date.slice(0, 10)}</span>
+            <span>~</span>
+            <span className="text-muted ms-1">{project.to_date.slice(0, 10)}</span>
           </Col>
           {isEditable && (
-            <Col xs lg="1">
-              <Button
-                variant="outline-info"
-                size="sm"
-                onClick={() => setIsEditing((prev) => !prev)}
-                className="mr-3"
-              >
-                편집
-              </Button>
-            </Col>
+            <>
+              <Col md="auto">
+                <Button
+                  variant="outline-info"
+                  size="sm"
+                  onClick={() => setIsEditing((prev) => !prev)}
+                  className="mr-3"
+                >
+                  편집
+                </Button>
+              </Col>
+              <Col md="auto">
+                <Button
+                  variant="outline-danger"
+                  size="sm"
+                  onClick={(e) => {if(window.confirm("해당 프로젝트를 삭제하시겠습니까?")){handleDelete(e)}}}
+                  className="mr-3"
+                >
+                  삭제
+                </Button>
+              </Col>
+            </>  
           )}
         </Row>
       </Card.Text>

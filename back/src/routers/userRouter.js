b5cb827fc13576c54a74d99a969eccee2e8a4c96
Hyejin Eom <hjeom.dev@gmail.com>
2022-03-25 00:01:36 +0900

    Comment: add userRouter & Chore: add yarn.lock

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -83,9 +83,7 @@ userAuthRouter.get("/userlist", login_required, async function (req, res, next) 
         const id = req.currentUserId;
         /* Pagination */
         const perPage = 10;
-        console.log(req.query);
         const page = Number(req.query.page) || 1;
-        console.log(`userlist 요청 ${page}`);
 
         const [usersCount, users] = await Promise.all([
             // 전체 사용자 수를 얻음

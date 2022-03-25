3f089a9208fe6a9426dd1b5f2fcb28382328772b
Hyejin Eom <hjeom.dev@gmail.com>
2022-03-25 17:05:29 +0900

    Fix: 놓친 주석처리

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -386,6 +386,6 @@ userAuthRouter.post("/login/google", async (req, res, next) => {
 //     const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 //     let result1= Math.random().toString(36).substring(0,num);       
 //     return result1;
-}
+// }
 
 export { userAuthRouter };
